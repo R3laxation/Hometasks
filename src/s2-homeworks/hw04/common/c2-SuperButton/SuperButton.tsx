28e57aca176c1c298c2b736bf0711622dbd4073e
R3laxation <memories@bk.ru>
2023-01-09 20:13:55 +0600

    Changed HW4 component

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,7 +17,14 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
+    const finalClassName =
+
+        s.button
+        + (disabled ? ' ' + s.disabled : '')
+        + (xType === 'red' ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : '')
+        + (className ? ' ' + className : '')
+
+
         // + (disabled
         //         ? ...
         //         : xType === 'red'
